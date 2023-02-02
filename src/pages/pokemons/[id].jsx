e9b33ebac7177461a66345db06272ae5d4dbae5e
luiznaiper <luiz_naiper@hotmail.com>
2023-02-02 08:26:15 -0300

    :construction: Add dynamic gen

--- src/pages/pokemons/[id].jsx
+++ src/pages/pokemons/[id].jsx
@@ -1,8 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Pokemon = ({ data }) => {
-  console.log(data);
+  const router = useRouter();
+  console.log(router);
+
+  if (router.isFallback) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <h1>{data.name}</h1>
@@ -13,11 +20,26 @@ const Pokemon = ({ data }) => {
   );
 };
 
-export const getServerSideProps = async ({ params }) => {
+export const getStaticProps = async ({ params }) => {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
   const data = await res.json();
 
   return { props: { data } };
 };
+
+export const getStaticPaths = async () => {
+  const paths = [{ params: { id: '1' } }];
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+// export const getServerSideProps = async ({ params }) => {
+//   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
+//   const data = await res.json();
+
+//   return { props: { data } };
+// };
 
 export default Pokemon;
